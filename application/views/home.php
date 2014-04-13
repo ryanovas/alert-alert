7c9c0c7d60245f4dc4f025d2be91399565e73768
djagula <dexterjagula@gmail.com>
2014-04-12 22:14:55 -0400

    Google maps code added

--- application/views/home.php
+++ application/views/home.php
@@ -25,6 +25,81 @@ header( 'Pragma: no-cache' );
 
 	<?php echo '<script src="' . base_url() . 'assets/js/jquery-1.11.0.min.js"></script>'; ?>
 
+	<!--Google Map-->
+	<style>
+    	html, body, #map-canvas {
+        height: 100%;
+        margin: 0px;
+        padding: 0px
+      	}
+    </style>
+    
+    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
+    
+    <script>
+		
+		var skyEvent = {};
+				
+		// Function for size of an object
+		Object.size = function(obj) {
+    		var size = 0, key;
+    		for (key in obj) {
+        		if (obj.hasOwnProperty(key)) size++;
+    		}
+    		return size;
+		};
+
+		var lat, lng;
+
+		// Function for creating a Sky Event
+		function createSkyEvent(lat, lng) {
+			var index = Object.size(skyEvent);
+			skyEvent[index] = {
+				center: new google.maps.LatLng(lat, lng)
+			};
+		}
+
+		// DUMMY COORDINATES
+		// createSkyEvent(43.7182713, -79.3777061);
+		// createSkyEvent(47.614848, -122.3358423);
+		// createSkyEvent(41.878113, -87.629798);
+		// createSkyEvent(40.714352, -74.005973);
+		// createSkyEvent(34.052234, -118.243684);
+
+		var eventCircle;
+
+		function initialize() {
+		  // Create the map
+		  var mapOptions = {
+		    zoom: 3,
+		    center: new google.maps.LatLng(37.09024, -95.712891),
+		    mapTypeId: google.maps.MapTypeId.TERRAIN,
+		    streetViewControl: false
+		  };
+
+		  var map = new google.maps.Map(document.getElementById('map-canvas'),
+		      mapOptions);
+
+		  for (var sEvent in skyEvent) {
+		    var addCircle = {
+		      strokeColor: '#FF0000',
+		      strokeOpacity: 0.8,
+		      strokeWeight: 2,
+		      fillColor: '#FF0000',
+		      fillOpacity: 0.35,
+		      map: map,
+		      center: skyEvent[sEvent].center,
+		      radius: 200000
+		    };
+		    // Add the circle for this coordinate to the map
+		    eventCircle = new google.maps.Circle(addCircle);
+		  }
+		}
+
+		google.maps.event.addDomListener(window, 'load', initialize);
+    </script>
+    <!--Google Map-->
+
 </head>
 
 <body>
@@ -47,13 +122,7 @@ header( 'Pragma: no-cache' );
 
 		<div class="displayCont">
 
-			<iframe
-			  width="100%"
-			  height="100%"
-			  frameborder="0" style="border:0"
-			  src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDuc2Lie8N0rCNGCqG2S2_rRPb5xiC_EcM
-			    &q=Space+Needle,Seattle+WA">
-			</iframe>
+			<div id="map-canvas"></div>
 
 		</div>
 
