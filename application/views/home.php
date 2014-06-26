761a071bc6ecd15cd20e95547042a2f63fd5ec5f
James Slifierz <james@sproutr.co>
2014-06-26 14:49:38 -0400

    progress

--- application/views/home.php
+++ application/views/home.php
@@ -1,9 +1,9 @@
 <div id="bgGrad">
 
-	<div id="wrapper">
+	<div id="wrapper" class="contentOpen">
 
 		<div id="contentCont">
-			<img src="../../assets/images/logo.png" alt="SkyWatch Logo">
+			<img src="../../assets/images/logo.png" alt="SkyWatch Logo" class="contentOpen">
 		</div>
 
 		<div id="whatIs">
@@ -14,9 +14,45 @@
 			<p>We built SkyWatch to tap into these networks and present to the world the ability to follow the cosmos in real-time. We created a Twitter-like interface to stream the events from the cosmos right into the palm of your hands. We’ve also provided access to Google Maps to plot where on Earth these observations are coming from, and access to Google Sky to plot where in the cosmos these events are taking place.</p>
 			<p>We have the goal of bringing SkyWatch to science centres, universities, and smartphones all around the world. For the first time in history, at the touch of a button, any child anywhere in the world could play witness to a supernova at the same time as an astrophysicist at NASA. We know that space is the next frontier and we hope that by providing everyone with access to the stars that they will one day aspire to reach for them.</p>
 		</div>
+
+		<div id="team">
+
+			<div class="bioCont">
+				<div class="bioHeader">
+					<h1>Dexter Jagula</h1>
+					<a href="dexterjagula@gmail.com" target="_blank"><span class="socialIcon">circleemail</span></a>
+					<a href="https://twitter.com/djagula" target="_blank"><span class="socialIcon">circletwitterbird</span></a>
+					<a href="https://www.linkedin.com/in/dexterjagula" target="_blank"><span class="socialIcon">circlelinkedin</span></a>
+				</div>
+				<div class="bio">
+					<p>Dexter is an entrepreneur and IT consultant with over 5 years of experience in various roles and industries. He holds a Bachelor’s in Software Engineering from the University of Ottawa, and a Master’s in Space Studies from the International Space University.</p>
+				</div>
+			</div>
+
+			<div id="imgWrapper">
+
+				<!-- <div class="imgCont"> -->
+					<img src="../../assets/images/dexter.jpg" alt="Dexter Jagula">
+				<!-- </div> -->
+				<!-- <div class="imgCont"> -->
+					<img src="../../assets/images/ryan.jpg" alt="Ryan Ovas">
+				<!-- </div> -->
+				<!-- <div class="imgCont"> -->
+					<img src="../../assets/images/roland.jpg" alt="Roland Sing">
+				<!-- </div> -->
+				<!-- <div class="imgCont"> -->
+					<img src="../../assets/images/stefan.jpg" alt="Stefan Sing">
+				<!-- </div> -->
+				<!-- <div class="imgCont"> -->
+					<img src="../../assets/images/james.jpg" alt="James Slifierz">
+				<!-- </div> -->
+
+			</div>
+
+		</div>
 			
 		<div id="emails">
-			<input type="text" name="email" placeholder="Your Email"><button>Get Updates</button>
+			<input type="text" name="email" placeholder="Your Email"><button>Sign Up for Private Beta</button>
 		</div>
 
 		<div id="menu">
@@ -41,25 +77,16 @@
 </div>
 
 <script type="text/javascript">
-
+	
+	var logo = $('#contentCont > img');
 	var whatIs = $('#whatIs');
 	var opt1 = $('#menu li:first-of-type');
 
-	texteditor(submit, textarea);
+	$(opt1).click(function() {
 
-	$('.emailMe').click(function() {
-
-
-		$('#openMessage').css('display','block');
-		$('#overlay').css('display','block');
-		openModal = true;
-
-	});
-
-	$('#emailSubmit').click(function() {
-
-		$('#emailSubmit').css('opacity', '0');
-		$('.loading').css('opacity', '1');
+		$(whatIs).addClass('open');
+		$('#wrapper').addClass('contentOpen');
+		$(logo).addClass('contentOpen');
 
 	});
 
